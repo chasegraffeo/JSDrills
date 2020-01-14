dd853d3632cce9d8767e2ba8391a40f37b834b96
chasegraffeo <chasegraffeo@gmail.com>
2020-01-14 16:19:43 -0600

    second

---
+++ 99line.js
@@ -0,0 +1,10 @@
+var friends = ["Liam", "Tanner", "Brie", "Endigo", "Chase"]
+for (var l = 0; l < friends.length; l++) {
+    for (i = 99; i >= 0; i--) {
+        if (i > 1) {
+            console.log(`${i} lines of code in the file, ${i} lines of code; ` + friends[l] + ` strikes one out, clears it all out, ${i-1} lines of code in the file`)
+        } else if (i == 1) {
+            console.log(`${i} line of code in the file, ${i} line of code; ` + friends[l] + ` strikes one out, clears it all out, ${i-1} lines of code in the file `)
+        }
+    }
+}
